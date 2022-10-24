8bc64319493459e1c785b86c919950e34ea87429
Jesse Liptak <jesse.liptak@unity3d.com>
2022-10-24 15:12:25 -0600

    Add query to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
+import { useGetOrdered } from "./queries/owen-wilson/owen-wilson-query";
 
 function App() {
   const [count, setCount] = useState(0);
+  const { isLoading, data } = useGetOrdered(6);
+
+  console.log(data);
 
   return (
     <div className="w-full h-full bg-gradient-to-r from-cyan-500 to-blue-500 ">
       Hello
+      {!isLoading && (
+        <video width="480" height="360" controls>
+          <source src={data.video["360p"]} type="video/mp4" />
+        </video>
+      )}
     </div>
   );
 }
