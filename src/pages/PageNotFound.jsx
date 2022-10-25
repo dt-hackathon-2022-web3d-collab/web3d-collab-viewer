d8ffe81233f094f114f4933ace54728465f47979
avery_garmaise <avery.garmaise@unity3d.com>
2022-10-25 16:36:10 -0400

    Reverted PageNotFound

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,11 +1,9 @@
-import CreateButton from "../components/CreateButton.jsx";
 
 export default function PageNotFound() {
   return (
     <div className="w-full h-full bg-gradient-to-r from-cyan-500 to-blue-500 flex items-center justify-center flex-col text-white">
       <h1>Hey there!</h1>
-      <p>Do you need a room?</p>
-      <CreateButton />
+      <p>Are you lost?</p>
     </div>
   );
 }
