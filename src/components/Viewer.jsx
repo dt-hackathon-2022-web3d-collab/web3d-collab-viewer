6f2260bf8fad2b2518f75a67815d7decce8c58d0
Xiaozhong Chen <xiaozhong.chen@unity3d.com>
2022-10-25 15:34:02 -0400

    fix: remove unused code

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -2,28 +2,9 @@ import { useEffect } from "react";
 
 const Viewer = () => {
     let cartier_api, context, viewer;
-    const timeFunctionsByName = {
-        setRealtime, setFixedTime, setFastForward
-    };
 
    useEffect(() => {
       setup();
-    
-      window.addEventListener("message", (event) => {
-        if (window === event.source) {
-          return;
-        }
-        const {method, args} = event.data;
-        switch (method) {
-          case 'setViewPoint':
-            setViewPoint(args[0]);
-            break;
-          case 'setWatchFunction':
-            timeFunctionsByName[args[0]]();
-            break;
-          default: break;
-        }
-      }, false);
    }, []);
 
     async function setup() {
@@ -47,32 +28,7 @@ const Viewer = () => {
         }, 1000)
       }
     }
-    
-    // window.parent.postMessage(['formaFrameInitialized', '0.0.1'], "*");
 
-    function onProductChanged(evt) {
-    }
-
-    async function setRealtime() {
-      await setup();
-      cartier_api?.setRealtime?.call(cartier_api);
-    }
-
-    async function setFixedTime() {
-      await setup();
-      cartier_api?.setFixedTime?.call(cartier_api);
-    }
-
-    async function setFastForward() {
-      await setup();
-      cartier_api?.setFastForward?.call(cartier_api);
-    }
-
-    async function setViewPoint(id) {
-      await setup();
-      cartier_api?.setViewPoint?.call(cartier_api, id);
-    }
-    
     return (
       <needle-engine>
           <div className="loading"></div>
