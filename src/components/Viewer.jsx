400018931da05d00a49e159a26ffe1d5e55a5025
tonyjreij <tony.jreij@unity3d.com>
2022-10-25 14:46:49 -0400

    Needleviewer part 1

---
+++ src/components/Viewer.jsx
@@ -0,0 +1,84 @@
+import { useEffect } from "react";
+
+const Viewer = () => {
+    let cartier_api, context, viewer;
+    const timeFunctionsByName = {
+        setRealtime, setFixedTime, setFastForward
+    };
+
+   useEffect(() => {
+      setup();
+    
+      window.addEventListener("message", (event) => {
+        if (window === event.source) {
+          return;
+        }
+        const {method, args} = event.data;
+        switch (method) {
+          case 'setViewPoint':
+            setViewPoint(args[0]);
+            break;
+          case 'setWatchFunction':
+            timeFunctionsByName[args[0]]();
+            break;
+          default: break;
+        }
+      }, false);
+   }, []);
+
+    async function setup() {
+      try {
+        if (viewer && context && cartier_api) return;
+        viewer = document.querySelector("needle-engine");
+        console.log({viewer, context: viewer.context})
+        if(!viewer) return;
+        context = viewer.context;
+        if(!context) return;
+
+        cartier_api = viewer.GameObject.findObjectOfType("CartierViewer");
+        viewer.removeEventListener("product-selected", onProductChanged);
+        viewer.addEventListener("product-selected", onProductChanged);
+      }
+      catch (err) {
+        console.error(err);
+        setTimeout(() => {
+          setup();
+        }, 1000)
+      }
+    }
+    
+    // window.parent.postMessage(['formaFrameInitialized', '0.0.1'], "*");
+
+    function onProductChanged(evt) {
+    }
+
+    async function setRealtime() {
+      await setup();
+      cartier_api?.setRealtime?.call(cartier_api);
+    }
+
+    async function setFixedTime() {
+      await setup();
+      cartier_api?.setFixedTime?.call(cartier_api);
+    }
+
+    async function setFastForward() {
+      await setup();
+      cartier_api?.setFastForward?.call(cartier_api);
+    }
+
+    async function setViewPoint(id) {
+      await setup();
+      cartier_api?.setViewPoint?.call(cartier_api, id);
+    }
+    
+    return (
+      <needle-engine>
+          <div className="loading"></div>
+              <div id="overlay" className="overlay ar desktop">
+          </div>
+      </needle-engine>
+    )
+}
+
+export default Viewer;
--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,16 +1,18 @@
 import Participants from "../components/Participants";
 import Toolbar from "../components/Toolbar";
+import Viewer from "../components/Viewer";
 import { useGetOrdered } from "../queries/owen-wilson/owen-wilson-query";
 
 const Room = () => {
   const { isLoading, data } = useGetOrdered(6);
-
-  console.log(data);
-
+  
   return (
     <div className="w-full h-full bg-gradient-to-r from-cyan-500 to-blue-500">
       <div className="w-3/4 mx-auto bg-yellow text-center">
         <Participants />
+      </div>
+      <div className="h-3/4 w-full flex justify-center items-center">
+        <Viewer />
       </div>
       <div className="absolute bottom-0 left-0">
         <Toolbar />
