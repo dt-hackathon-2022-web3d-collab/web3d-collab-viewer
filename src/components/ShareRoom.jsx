d707c5e3ed0caf78005a17e5139054c297594f8b
Casey Yee <caseyyee.ca@gmail.com>
2022-10-25 16:19:33 -0400

    Share room button, copy to clipboard

---
+++ src/components/ShareRoom.jsx
@@ -0,0 +1,18 @@
+import { useParams } from "react-router-dom";
+
+export default () => {
+  const { roomId } = useParams();
+
+  const handleClick = () => {
+    navigator.clipboard.writeText(`${window.location.host}/rooms/${roomId}`)
+  }
+
+  return (
+    <button
+      onClick={handleClick}
+      className="bg-white hover:bg-blue-700 font-bold py-2 px-4 rounded"
+    >
+      Share room
+    </button>
+  )
+}
--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,6 +1,7 @@
 import NameModal from "../components/NameModal.jsx";
 import Participants from "../components/Participants";
 import Toolbar from "../components/Toolbar";
+import ShareRoom from "../components/ShareRoom";
 import { useWebSocket } from "../hooks/useWebSocket/useWebSocket";
 import { useParams } from "react-router-dom";
 
@@ -21,8 +22,11 @@ const Room = () => {
       <div className="w-3/4 mx-auto bg-yellow text-center">
         <Participants />
       </div>
-      <div className="absolute bottom-0 left-0">
+      <div className="absolute bottom-2 left-2">
         <Toolbar />
+      </div>
+      <div className="absolute bottom-2 right-2">
+        <ShareRoom/>
       </div>
     </div>
   );
