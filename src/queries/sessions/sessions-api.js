79738032a79adaab4b50f7001557ca45fbce3694
avery_garmaise <avery.garmaise@unity3d.com>
2022-10-25 15:52:27 -0400

    Added session getter queries

--- src/queries/sessions/sessions-api.js
+++ src/queries/sessions/sessions-api.js
@@ -9,3 +9,15 @@ export const createSession = async (body) => {
 
   return response.json();
 };
+
+export const getAllSessions = async () => {
+  const response = await fetch(`http://${url}/v1/sessions`, { method: "GET" });
+  return response.json();
+};
+
+export const getSession = async ({ sessionId }) => {
+  const response = await fetch(`http://${url}/v1/sessions/${sessionId}`, {
+    method: "GET",
+  });
+  return response.json();
+};
