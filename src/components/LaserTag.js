44a6f05498c2342824943114e5b229f51d712188
Xiaozhong Chen <xiaozhong.chen@unity3d.com>
2022-10-26 15:41:22 -0400

    feat: add laser tag

---
+++ src/components/LaserTag.js
@@ -0,0 +1,33 @@
+export class LaserTag {
+  constructor(context) {
+    this.context = context;
+    this.THREE = context.THREE;
+    const THREE = this.THREE;
+    this.beam = new THREE.Mesh(
+      new THREE.CylinderGeometry( 1, 1, 1, 8 ),
+      new THREE.MeshBasicMaterial( {color: 0x000000} )
+    );
+    this.beam.scale.set(0.01, 1, 0.01);
+    context.scene.add(this.beam);
+  }
+
+  point(from, to) {
+    const {beam, THREE} = this;
+    beam.position.copy(new THREE.Vector3().addVectors(from, to).multiplyScalar(0.5));
+    beam.scale.setY(from.distanceTo(to));
+    const dir = new THREE.Vector3().subVectors(to, from).normalize();
+    beam.setRotationFromQuaternion(new THREE.Quaternion().setFromUnitVectors(new THREE.Vector3(0, 1, 0), dir));
+  }
+
+  color(cssColorStr) {
+    this.beam.material.color.setStyle(cssColorStr);
+  }
+
+  show() {
+    this.beam.visible = true;
+  }
+
+  hide() {
+    this.beam.visible = false;
+  }
+}
