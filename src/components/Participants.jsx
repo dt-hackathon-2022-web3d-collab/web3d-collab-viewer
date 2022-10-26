12877465feb2b73d27e8c06f27fa87e401e15e85
Casey Yee <caseyyee.ca@gmail.com>
2022-10-26 14:03:07 -0400

    fixes key prop

--- src/components/Participants.jsx
+++ src/components/Participants.jsx
@@ -8,10 +8,7 @@ const Participants = ({ participants }) => {
     const thumbImage = avatarArray[colourIndex];
     return (
       <div key={`participant-${index}`} className="inline-block">
-        <div
-          key={`participant-${index}`}
-          className={`p-2 m-1 ${colour} rounded-full`}
-        >
+        <div className={`p-2 m-1 ${colour} rounded-full`}>
           <div
             className="rounded-full bg-black w-16 h-16 bg-cover"
             style={{ backgroundImage: `url('${thumbImage}')` }}
