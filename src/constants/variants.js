32b0f17762efbc84e9da7baa301e8d36c22b2958
Casey Yee <caseyyee.ca@gmail.com>
2022-10-27 18:29:11 -0400

    variants imports

---
+++ src/constants/variants.js
@@ -0,0 +1,29 @@
+import thumb1 from "../assets/variants/1.png";
+import thumb2 from "../assets/variants/2.png";
+import thumb3 from "../assets/variants/3.png";
+import thumb4 from "../assets/variants/4.png";
+import thumb5 from "../assets/variants/5.png";
+import thumb6 from "../assets/variants/6.png";
+import thumb7 from "../assets/variants/7.png";
+import thumb8 from "../assets/variants/8.png";
+import thumb9 from "../assets/variants/9.png";
+import thumb10 from "../assets/variants/10.png";
+import thumb11 from "../assets/variants/11.png";
+import thumb12 from "../assets/variants/12.png";
+import thumb13 from "../assets/variants/13.png";
+
+export const variantsArray = [
+  thumb1,
+  thumb2,
+  thumb3,
+  thumb4,
+  thumb5,
+  thumb6,
+  thumb7,
+  thumb8,
+  thumb9,
+  thumb10,
+  thumb11,
+  thumb12,
+  thumb13,
+];
