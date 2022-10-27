a668d3f2ac53efd58eed5f2d1043a9e151eac211
avery_garmaise <avery.garmaise@unity3d.com>
2022-10-27 13:04:19 -0400

    Added char keys to toolbar buttons

---
+++ src/components/CharKey.jsx
@@ -0,0 +1,5 @@
+export const CharKey = ({ children }) => (
+  <div className="mx-auto px-2 border border-gray-400 rounded shrink">
+    {children}
+  </div>
+);
--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,5 +1,6 @@
 import { useCallback, useEffect } from "react";
 import { CameraRotate, ChatCircleDots, ScribbleLoop } from "phosphor-react";
+import { CharKey } from "./CharKey.jsx";
 import { RoundedButton } from "./RoundedButton";
 import modes from "../constants/modes.js";
 
@@ -8,6 +9,13 @@ const keyCodes = {
   83: modes.annotate,
   68: modes.point,
 };
+
+const modeKeys = Object.fromEntries(
+  Object.entries(keyCodes).map(([code, mode]) => [
+    mode,
+    String.fromCharCode(code),
+  ])
+);
 
 const buttons = {
   [modes.view]: <CameraRotate size={30} />,
@@ -33,13 +41,15 @@ const Toolbar = ({ mode, onModeChanged }) => {
   return (
     <div className="flex">
       {Object.entries(buttons).map(([key, icon]) => (
-        <RoundedButton
-          key={key}
-          onClick={() => onModeChanged(key)}
-          selected={mode === key}
-        >
-          {icon}
-        </RoundedButton>
+        <div key={key} className="flex flex-col">
+          <CharKey>{modeKeys[key]}</CharKey>
+          <RoundedButton
+            onClick={() => onModeChanged(key)}
+            selected={mode === key}
+          >
+            {icon}
+          </RoundedButton>
+        </div>
       ))}
     </div>
   );
