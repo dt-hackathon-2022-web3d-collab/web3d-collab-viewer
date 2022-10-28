2fe1f8c418e7339bbddecd40a94532009aed4b55
Casey Yee <caseyyee.ca@gmail.com>
2022-10-27 21:20:31 -0400

    Annotation styling

--- src/components/Annotations/AnnotationItem.jsx
+++ src/components/Annotations/AnnotationItem.jsx
@@ -8,13 +8,22 @@ export const AnnotationItem = ({ onClick, ...annotation }) => {
   };
 
   return (
-    <div
-      className="bg-black/10 mb-2 rounded border border-white px-2"
-      onClick={handleOnClick}
-    >
-      {resolved && <del>{message}</del>}
-      {!resolved && message}
-      <br /> - {user.name}, {moment(new Date(user.updatedAt)).fromNow()}
+    <div className="px-2" onClick={handleOnClick}>
+      <div className="bg-white p-3 drop-shadow-md">
+        {resolved && <del>{message}</del>}
+        {!resolved && message}
+      </div>
+      <div class="w-10 overflow-hidden inline-block transform rotate-180">
+        <div class=" h-5 w-5 bg-white rotate-45 transform origin-bottom-left"></div>
+      </div>
+
+      <div className="flex flex-col">
+        <div>{user.name}</div>
+
+        <div className="text-sm">
+          {moment(new Date(user.updatedAt)).fromNow()}
+        </div>
+      </div>
     </div>
   );
 };
