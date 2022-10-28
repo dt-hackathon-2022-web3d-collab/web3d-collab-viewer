246fcf5c9f377feb8e023eb13bed294c19870c28
Xiaozhong Chen <xiaozhong.chen@unity3d.com>
2022-10-27 21:53:38 -0400

    style: more avatars!

--- src/constants/avatars.js
+++ src/constants/avatars.js
@@ -4,7 +4,7 @@ import thumb3 from "../assets/avatars/3.png";
 import thumb4 from "../assets/avatars/4.jpg";
 import thumb5 from "../assets/avatars/5.jpg";
 import thumb6 from "../assets/avatars/6.png";
-import thumb7 from "../assets/avatars/7.webp";
+import thumb7 from "../assets/avatars/7.jpg";
 import thumb8 from "../assets/avatars/8.jpg";
 import thumb9 from "../assets/avatars/9.jpg";
 import thumb10 from "../assets/avatars/10.png";
@@ -15,6 +15,12 @@ import thumb14 from "../assets/avatars/14.jpg";
 import thumb15 from "../assets/avatars/15.png";
 import thumb16 from "../assets/avatars/16.jpg";
 import thumb17 from "../assets/avatars/17.png";
+import thumb18 from "../assets/avatars/18.jfif";
+import thumb19 from "../assets/avatars/19.png";
+import thumb20 from "../assets/avatars/20.png";
+import thumb21 from "../assets/avatars/21.jpg";
+import thumb22 from "../assets/avatars/22.jpg";
+import thumb23 from "../assets/avatars/23.jpg";
 
 export const avatarArray = [
   thumb1,
@@ -34,4 +40,10 @@ export const avatarArray = [
   thumb15,
   thumb16,
   thumb17,
+  thumb18,
+  thumb19,
+  thumb20,
+  thumb21,
+  thumb22,
+  thumb23,
 ];
